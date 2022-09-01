dbc69990bcc4000f3706b94341a31dfed53041aa
Sergey Novikov <vizzeb@gmail.com>
2022-09-01 10:18:01 +0300

    final

--- src/main/java/com/example/spring_boot_rest/UserRepository.java
+++ src/main/java/com/example/spring_boot_rest/UserRepository.java
@@ -10,7 +10,7 @@ public class UserRepository {
     public List<Authorities> getUserAuthorities(String user, String password) {
         List<Authorities> authorities = new ArrayList<>();
 
-        if(user.equals("admin") && password.equals("admin")){
+        if (user.equals("admin") && password.equals("admin")) {
             authorities.add(Authorities.WRITE);
             authorities.add(Authorities.DELETE);
             authorities.add(Authorities.READ);
